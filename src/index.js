1345cd6f46fda2a00b6b75a4469e8ce1f4b1900f
Su <gzqd201802@163.com>
2020-01-10 18:37:24 +0800

    添加less文件处理

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 // 这个语法是导入一个 style.css 文件
 import './style.css';
+// 通过 import 语法导入一个 style.less 文件
+import './style.less';
 
 // 写入到html的内容
 const element = document.createElement("div");
 
-element.innerHTML =  `<div class="red">Hello 老班</div>`;
+element.innerHTML =  `
+    <div class="red">Hello 老班</div>
+    <div class="blue">hello webpack and less</div>
+`;
 
 document.body.appendChild(element);
--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,22 @@ module.exports = {
     module: {
         // 规则
         rules: [
+            // 第一个规则 用于处理 css 文件
             {
                 // 检验匹配那类文件.
                 test: /\.css$/,
                 // 需要使用哪些 loader处理 css 文件
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            // 第二个规则 用于处理 less 文件
+            {
+                test: /\.less$/,		// 匹配less扩展名文件
+                use:[				
+                    'style-loader',		// 把less代码写入到网页中
+                    'css-loader',		// 读取less的代码
+                    'less-loader'		// 解释编译less代码
+                ]	
+            },
         ]
     },
 };
