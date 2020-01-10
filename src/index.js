586bc6c48eb7db9f197b78e91095c53220567d18
Su <gzqd201802@163.com>
2020-01-10 18:28:04 +0800

    css文件打包

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// 这个语法是导入一个 style.css 文件
+import './style.css';
+
 // 写入到html的内容
 const element = document.createElement("div");
 
-element.innerHTML =  `<div>Hello 老班</div>`;
+element.innerHTML =  `<div class="red">Hello 老班</div>`;
 
 document.body.appendChild(element);
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,24 @@ const path = require('path');
 // 导出
 module.exports = {
     // 入口文件
-    entry: './src/index.js',					
+    entry: './src/index.js',
     // 输出位置
     output: {
         // 打包后到那个文件夹
-        path: path.join(__dirname, 'dist'),  
+        path: path.join(__dirname, 'dist'),
         // 打包后的文件名
         filename: 'bundle.js',
-    }
+    },
+    // 模块 组件
+    module: {
+        // 规则
+        rules: [
+            {
+                // 检验匹配那类文件.
+                test: /\.css$/,
+                // 需要使用哪些 loader处理 css 文件
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
 };
